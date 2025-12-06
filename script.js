b9a7f1cde3b2f1e88f9cefb914ea22af51652254
Sukhwinder Singh <sukhamallan@gmail.com>
2025-12-06 20:37:58 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Small interactions & utility
-document.addEventListener('DOMContentLoaded', () => {
-  const year = document.getElementById('year');
-  if (year) year.textContent = new Date().getFullYear();
-});
